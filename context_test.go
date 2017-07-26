2a1dbf085d37e8585f987fce18e46444365ffec7
yangglive <yangglive@icloud.com>
2017-07-27 03:18:37 +0800

    add test file

---
+++ context_test.go
@@ -0,0 +1,20 @@
+package cart
+
+import "testing"
+
+// Params
+func TestParams(t *testing.T) {
+	ps := Params{
+		Param{"param1", "value1"},
+		Param{"param2", "value2"},
+		Param{"param3", "value3"},
+	}
+	for i := range ps {
+		if val, _ := ps.Get(ps[i].Key); val != ps[i].Value {
+			t.Errorf("Wrong value for %s: Got %s; Want %s", ps[i].Key, val, ps[i].Value)
+		}
+	}
+	if val, _ := ps.Get("noKey"); val != "" {
+		t.Errorf("Expected empty string for not found key; got: %s", val)
+	}
+}
