d02f968516d851e7cd10f6463c1c48384f2e9c05
yangglive <yangglive@icloud.com>
2017-07-27 03:13:19 +0800

    start up

--- context.go
+++ context.go
@@ -1,1 +1,25 @@
 package cart
+
+import "net/http"
+
+type Context struct {
+	Request   *http.Request
+	Writer    http.ResponseWriter
+	Keys     map[string]interface{}
+}
+
+type Param struct {
+	Key   string
+	Value string
+}
+
+type Params []Param
+
+func (ps Params) Get(name string) (string, bool) {
+	for _, entry := range ps {
+		if entry.Key == name {
+			return entry.Value, true
+		}
+	}
+	return "", false
+}
--- utils.go
+++ utils.go
@@ -1,1 +1,5 @@
 package cart
+
+type Next func()
+type ComposeHandle func(*Context, Next) Next
+
