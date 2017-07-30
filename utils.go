2b59bb00fa83d60c76e349313eadd7d1dbb2874f
yangglive <yangglive@icloud.com>
2017-07-30 22:14:31 +0800

    add html json xml text support

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package cart
 import (
 	"path"
 	"os"
+	"encoding/xml"
 )
 
 type Next func()
@@ -12,6 +13,29 @@ type HandlerFinal func(*Context)
 type HandlerCompose func(*Context, Next) Next
 
 type H map[string]interface{}
+// MarshalXML allows type H to be used with xml.Marshal
+func (h H) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	start.Name = xml.Name{
+		Space: "",
+		Local: "map",
+	}
+	if err := e.EncodeToken(start); err != nil {
+		return err
+	}
+	for key, value := range h {
+		elem := xml.StartElement{
+			Name: xml.Name{Space: "", Local: key},
+			Attr: []xml.Attr{},
+		}
+		if err := e.EncodeElement(value, elem); err != nil {
+			return err
+		}
+	}
+	if err := e.EncodeToken(xml.EndElement{Name: start.Name}); err != nil {
+		return err
+	}
+	return nil
+}
 
 func resolveAddress(addr []string) string {
 	switch len(addr) {
